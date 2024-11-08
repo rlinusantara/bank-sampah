97b015ed76117668696a286f5944844cf8352fbd
unknown <muizzuddin334@gmail.com>
2024-11-08 08:56:54 +0700

    w

--- src/app/components/sidebar.js
+++ src/app/components/sidebar.js
@@ -1,32 +1,52 @@
-import { ChevronFirst, ChevronLast,Database,UserRoundPlus,TicketPlus,HandCoins,Banknote, LayoutDashboard, Egg } from "lucide-react";
+import {
+  ChevronFirst,
+  ChevronLast,
+  Database,
+  UserRoundPlus,
+  TicketPlus,
+  HandCoins,
+  Banknote,
+  LayoutDashboard,
+  Egg,
+} from "lucide-react";
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 export default function SidebarAdmin() {
   const [expanded, setExpanded] = useState();
-
-  // eslint-disable-next-line react/prop-types
-  const SidebarItem = ({ icon, text, active, alert ,link}) => {
+  const SidebarItem = ({ icon, text, active, alert, link }) => {
     return (
-    <Link href={link}>
-      <li className={`relative flex items-center p-2 mb-1 h-12 lg:h-16 rounded-md cursor-pointer transition-colors group ${
-        active
-          ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
-          : "hover:bg-indigo-50 text-gray-600"
-      }`}>
-        {icon}
-        <span className={`overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}`}>
-          {text}
-        </span>
-        {alert && (
-          <div className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? "" : "top-2"}`}></div>
-        )}
-        {!expanded && (
-          <div className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}>
+      <Link href={link}>
+        <li
+          className={`relative flex items-center p-2 mb-1 h-12 lg:h-16 rounded-md cursor-pointer transition-colors group ${
+            active
+              ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
+              : "hover:bg-indigo-50 text-gray-600"
+          }`}
+        >
+          {icon}
+          <span
+            className={`overflow-hidden transition-all ${
+              expanded ? "w-52 ml-3" : "w-0"
+            }`}
+          >
             {text}
-          </div>
-        )}
-      </li>
+          </span>
+          {alert && (
+            <div
+              className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${
+                expanded ? "" : "top-2"
+              }`}
+            ></div>
+          )}
+          {!expanded && (
+            <div
+              className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm invisible opacity-20 -translate-x-3 transition-all group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}
+            >
+              {text}
+            </div>
+          )}
+        </li>
       </Link>
     );
   };
@@ -41,28 +61,61 @@ export default function SidebarAdmin() {
               alt="logo"
               width={500}
               height={500}
-              className={`overflow-hidden transition-all ${expanded ? "w-24" : "w-0"}`}
+              className={`overflow-hidden transition-all ${
+                expanded ? "w-24" : "w-0"
+              }`}
             />
             <button
               onClick={() => setExpanded((curr) => !curr)}
               className="p-1.5 rounded-lg text-black bg bg-indigo-100"
             >
-              {expanded ? <ChevronFirst size={30}/> : <ChevronLast size={30}/>}
+              {expanded ? (
+                <ChevronFirst size={30} />
+              ) : (
+                <ChevronLast size={30} />
+              )}
             </button>
           </div>
 
           <ul className="flex-1 px-3">
-            <SidebarItem icon={<LayoutDashboard size={30}/>} text="Dashboard" link="/dashboard"/>
-            <SidebarItem icon={<TicketPlus size={30} />} text="Setoran Masuk" link="/data-nasabah" />
-            <SidebarItem icon={<HandCoins size={30} />} text="Setoran Keluar" link="/data-nasabah" />
-            <SidebarItem icon={<Database size={30} />} text="Data Nasabah" link="/data-nasabah" />
-            <SidebarItem icon={<UserRoundPlus size={30} />} text="Tambah Nasabah" link="/data-nasabah" />
-            <SidebarItem icon={<Banknote size={30}/>} text="Harga Sampah" link="/data-nasabah" />
-            <SidebarItem icon={<Egg size={30}/>} text="Pendataan Telur BSF" link="/data-nasabah" />
+            <SidebarItem
+              icon={<LayoutDashboard size={30} />}
+              text="Dashboard"
+              link="/dashboard"
+            />
+            <SidebarItem
+              icon={<TicketPlus size={30} />}
+              text="Setoran Masuk"
+              link="/data-nasabah"
+            />
+            <SidebarItem
+              icon={<HandCoins size={30} />}
+              text="Setoran Keluar"
+              link="/data-nasabah"
+            />
+            <SidebarItem
+              icon={<Database size={30} />}
+              text="Data Nasabah"
+              link="/data-nasabah"
+            />
+            <SidebarItem
+              icon={<UserRoundPlus size={30} />}
+              text="Tambah Nasabah"
+              link="/data-nasabah"
+            />
+            <SidebarItem
+              icon={<Banknote size={30} />}
+              text="Harga Sampah"
+              link="/data-nasabah"
+            />
+            <SidebarItem
+              icon={<Egg size={30} />}
+              text="Pendataan Telur BSF"
+              link="/data-nasabah"
+            />
           </ul>
         </nav>
       </aside>
     </div>
-
   );
 }
