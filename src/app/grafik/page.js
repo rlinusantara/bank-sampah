22015ca6ba13ed3d58b7bf32eb7839eec966a0d5
unknown <muizzuddin334@gmail.com>
2024-11-15 18:00:08 +0700

    grafik tabungan customer

--- src/app/grafik/page.js
+++ src/app/grafik/page.js
@@ -1,12 +1,23 @@
+import HorizontalBarChart from "../components/barChart";
 import Layout from "../components/layout";
+
 const Grafik = () => {
-    return (
-        <>
-        <Layout>
-            <h1>INI HALAMAN GRAFIK</h1>
-        </Layout>
-        </>
-    );
-}
- 
+  return (
+    <>
+      <Layout>
+        <section className="px-4 mt-10 mb-20">
+          <HorizontalBarChart />
+        </section>
+
+        <section className="flex justify-end w-full px-10 mb-10 cursor-pointer">
+          <section className="w-24 border flex flex-col items-center justify-center rounded-sm border-gray-300">
+            <img src="/excel.png" alt="exel" />
+            <p className="font-medium text-sm">Export ke excel</p>
+          </section>
+        </section>
+      </Layout>
+    </>
+  );
+};
+
 export default Grafik;
