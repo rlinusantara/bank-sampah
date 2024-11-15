228e172bb0255ae7d24908b9c82766d1584bb9a6
unknown <muizzuddin334@gmail.com>
2024-11-15 18:44:57 +0700

    export excel

--- src/app/grafik/page.js
+++ src/app/grafik/page.js
@@ -1,7 +1,21 @@
+"use client";
 import HorizontalBarChart from "../components/barChart";
 import Layout from "../components/layout";
+import * as XLSX from "xlsx";
 
 const Grafik = () => {
+  const exportToExcel = () => {
+    const wb = XLSX.utils.book_new();
+
+    const data = [
+      { name: "Alice", age: 25, city: "New York" },
+      { name: "Bob", age: 30, city: "Los Angeles" },
+      { name: "Charlie", age: 35, city: "Chicago" },
+    ];
+    const ws = XLSX.utils.json_to_sheet(data);
+    XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+    XLSX.writeFile(wb, "data.xlsx");
+  };
   return (
     <>
       <Layout>
@@ -9,7 +23,10 @@ const Grafik = () => {
           <HorizontalBarChart />
         </section>
 
-        <section className="flex justify-end w-full px-10 mb-10 cursor-pointer">
+        <section
+          className="flex justify-end w-full px-10 mb-10 cursor-pointer"
+          onClick={exportToExcel}
+        >
           <section className="w-24 border flex flex-col items-center justify-center rounded-sm border-gray-300">
             <img src="/excel.png" alt="exel" />
             <p className="font-medium text-sm">Export ke excel</p>
