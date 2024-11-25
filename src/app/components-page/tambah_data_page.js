6725a7a82d7db77ece927b2dbe5302009183c1d6
unknown <muizzuddin334@gmail.com>
2024-11-25 23:17:26 +0700

    fix setoran masuk page

--- src/app/components-page/tambah_data_page.js
+++ src/app/components-page/tambah_data_page.js
@@ -1,14 +1,16 @@
+import AdminLayout from "../components/adminLayout";
 import FormInputSampah from "../components/formSampah";
-import Layout from "../components/layout";
-const TambahDataPage = ({ nasabah = [], hargaSatuan = 0 }) => {
+const TambahDataPage = ({ nasabah = [], hargaSatuan = 0, isLogin = false }) => {
   return (
     <>
-      <Layout>
-        <h1 className="text-center xl:text-2xl font-bold p-3 font-nunito">
-          FORM INPUT DATA BANK SAMPAH
-        </h1>
-        <FormInputSampah nasabah={nasabah} hargaSatuan={hargaSatuan} />
-      </Layout>
+      <AdminLayout isLogin={isLogin}>
+        <section className="ml-20">
+          <h1 className="text-center xl:text-2xl font-bold p-3 font-nunito">
+            FORM INPUT DATA BANK SAMPAH
+          </h1>
+          <FormInputSampah nasabah={nasabah} hargaSatuan={hargaSatuan} />
+        </section>
+      </AdminLayout>
     </>
   );
 };
