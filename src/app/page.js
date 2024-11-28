17c85f9e30daeda5c8577d43b9e21f0743215ca2
unknown <muizzuddin334@gmail.com>
2024-11-28 09:06:09 +0700

    fix link prefetch

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+import connectDB from "@/db/connection";
 import Main from "./components/home";
-export default function Home() {
+export default async function Home() {
+  await connectDB();
+
   return (
     <>
-    <Main/>
+      <Main />
     </>
   );
 }
