3c0cc694d63bd3e2492990ea99df8092b4fdb720
unknown <muizzuddin334@gmail.com>
2024-12-02 08:45:55 +0700

    menggunakan fetch

--- src/app/components/formSampah2.js
+++ src/app/components/formSampah2.js
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import Select from "react-select";
-import axios from "axios";
 import { useRef } from "react";
 import { CircleCheckBig } from "lucide-react";
 
@@ -25,16 +24,25 @@ const FormSampah2 = ({ nasabah = [], hargaSatuan }) => {
       setPopUp(true);
       setPopUpisLoading(true);
 
-      const res = await axios.post("/api/setoran-masuk", {
-        id_nasabah: _id.valueOf().value,
-        tanggal_setoran: tanggal,
-        sampah_halus: jumlahSampahHalus,
-        sampah_kasar: jumlahSampahKasar,
-        jenis_sampah: jenisSampah,
+      const res = await fetch("/api/setoran-masuk", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({
+          id_nasabah: _id.valueOf().value,
+          tanggal_setoran: tanggal,
+          sampah_halus: jumlahSampahHalus,
+          sampah_kasar: jumlahSampahKasar,
+          jenis_sampah: jenisSampah,
+        }),
       });
 
+      const data = await res.json();
+
       setPopUpisLoading(false);
-      setMsg(res.data?.message);
+      setMsg(data?.message);
       form.current.reset();
       selectRef.current.clearValue();
     } catch (error) {
