6ab7fc64031fb452579c8da60cb1bb5669ce92fa
Hannn <ilhanjuliansyah3@gmail.com>
2024-12-13 10:18:00 +0700

    Update grafik.js

--- src/app/components/grafik.js
+++ src/app/components/grafik.js
@@ -1,46 +1,13 @@
 import { Line, Bar, Pie } from 'react-chartjs-2';
 import { Chart as ChartJS } from 'chart.js/auto';
 const Grafik = () => {
-    const lineChartData = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-        datasets: [
-          {
-            label: 'My First Dataset',
-            data: [65, 59, 80, 81, 56, 70],
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1,
-          },
-        ],
-      };
     
       const barChartData = {
-        labels: ['Red', 'Blue', 'Yellow'],
+        labels: ['January', 'February', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'],
         datasets: [
           {
-            label: 'My First Dataset',
-            data: [120, 50, 100],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      };
-    
-      const pieChartData = {
-        labels: ['Red', 'Blue', 'Yellow'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [12, 19, 3],
+            label: 'Grafik Setoran Sampah Bank Sampah',
+            data: [120, 50, 34,50,90,60,103,220,90,100,80,85],
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
@@ -58,20 +25,19 @@ const Grafik = () => {
     
     return (
         <>
-            <div className="text-black font-nunito">
-                <div>
-                    <h2>Line Chart</h2>
-                    <Line data={lineChartData} />
-                </div>
-                <div>
-                    <h2>Bar Chart</h2>
-                    <Bar data={barChartData} />
-                </div>
-                <div>
-                    <h2>Pie Chart</h2>
-                    <Pie data={pieChartData} />
-                </div>
-            </div>
+        <div className="mt-4">
+        <div className="flex">
+          <h1 className="p-1 xl:text-lg">Pilih tahun : </h1>
+          <select className="p-1 w-20">
+            <option value="" key="">
+              Pilih
+            </option>
+          </select>
+        </div>
+        <div>
+        <Bar data={barChartData} />
+        </div>
+      </div>
         </>
     );
 }
